9cdb290773da5639a74f27676e27a3b112f567f1
Mona Lisa <ssbyrne89@gmail.com>
2020-05-24 20:08:16 -0600

    updates

--- app/chinook_queries.py
+++ app/chinook_queries.py
@@ -4,9 +4,10 @@ import sqlite3
 
 # construct a path to wherever your database exists
 #DB_FILEPATH = "chinook.db"
-DB_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "data" "chinook.db")
+DB_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "data", "chinook.db")
 
 connection = sqlite3.connect(DB_FILEPATH)
+connection.row_factory = sqlite3.Row
 print("CONNECTION:", connection)
 
 cursor = connection.cursor()
@@ -16,6 +17,8 @@ query = "SELECT * FROM customers LIMIT 3;"
 
 #result = cursor.execute(query)
 #print("RESULT", result) #> returns cursor object w/o results (need to fetch the results)
+
+
 
 result2 = cursor.execute(query).fetchall()
 print("RESULT 2", result2)
